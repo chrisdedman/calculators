2d16031acd9c062580a59a13071f5f607edcfde2
Chris Dedman-Rollet <61106361+chrisdedman@users.noreply.github.com>
2023-06-26 10:45:47 -0400

    refactoring

--- calculator.ex
+++ calculator.ex
@@ -2,7 +2,27 @@ defmodule Calculator do
     @moduledoc """
     Provide mathematical calculation from user input
     """
+    @operators ~w(+ - * / ^ %)
+    
+    @doc """
+    Calculate x and y according to the user choice operant
+    """
+    def calculate(x, operator, y) when is_number(x) and is_number(y) and operator in @operators,
+    do: execute(x, operator, y)
+    # handles invalid operator
+    def calculate(_, operator, _) when operator not in @operators, do: {:error, "Invalid operator"}
+    def calculate(_, _, _), do: {:error, "Invalid arguments! Ex. `calculate(5, +, 5)`"}
+     # executes the calculation
+    defp execute(x, "+", y), do: x + y
+    defp execute(x, "-", y), do: x - y
+    defp execute(x, "*", y), do: x * y
+    defp execute(_, "/", 0), do: {:error, "Division by zero"}
+    defp execute(x, "/", y), do: x / y
+    defp execute(x, "^", y), do: :math.pow(x, y)
+    defp execute(x, "%", y), do: rem(x, y)
+end
 
+defmodule Main do
     @doc """
     Get user input for the numbers to be calculated
     """
@@ -12,8 +32,7 @@ defmodule Calculator do
             elem(Float.parse(number), 0)
         else
             nil
-        end
-        
+        end 
     end
 
     @doc """
@@ -24,28 +43,17 @@ defmodule Calculator do
         |> String.trim()
     end
 
-    @doc """
-    Calculate x and y according to the user choice operant
-    """
-    def calculate(x, "+", y), do: x + y
-    def calculate(x, "-", y), do: x - y
-    def calculate(x, "*", y), do: x * y
-    def calculate(x, "/", y), do: x / y
-    def calculate(x, "^", y), do: :math.pow(x, y)
-    def calculate(x, "%", y), do: rem x, y
-    def calculate(_, _, _), do: IO.puts("Invalid operator")
-end
-
-defmodule Main do
-    def main do
+    def run do
         IO.puts("\t==============================\n")
         IO.puts("\t Welcome to ElixirCalculator! \n")
         IO.puts("\t==============================\n\n")
-        x       = Calculator.get_user_input 
-        operant = Calculator.get_operator
-        y       = Calculator.get_user_input 
+        x        = get_user_input()
+        operator = get_operator()
+        y        = get_user_input()
 
-        result = Calculator.calculate(x, operant, y)
-        IO.puts("#{x} #{operant} #{y} = #{result}")
+        case Calculator.calculate(x, operator, y) do
+            {:error, message} -> IO.puts "Error: #{message}"
+            result -> IO.puts "Result: #{x} #{operator} #{y} = #{result}"
+        end
     end
 end
